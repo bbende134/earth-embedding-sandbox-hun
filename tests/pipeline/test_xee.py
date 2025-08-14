5ae0793de566b01b4dbc0f4f9e7eda464a760af5
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2025-08-14 21:57:24 +0100

    fix - test formatting

--- tests/pipeline/test_xee.py
+++ tests/pipeline/test_xee.py
@@ -1,22 +1,22 @@
-import xarray as xr
 import ee
+import pyproj
+import xarray as xr
+import zarr
 from shapely.geometry import Polygon
 from shapely.ops import transform
-import pyproj
-import numpy as np
-import zarr
+
 
 def test_clip_small_dataset(ee_initialize):
-
-    aoi = Polygon([[-1.406189579662267, 51.80481237120908],
-          [-1.406189579662267, 51.70662720111915],
-          [-1.171356815990392, 51.70662720111915],
-          [-1.171356815990392, 51.80481237120908]]
+    aoi = Polygon(
+        [
+            [-1.406189579662267, 51.80481237120908],
+            [-1.406189579662267, 51.70662720111915],
+            [-1.171356815990392, 51.70662720111915],
+            [-1.171356815990392, 51.80481237120908],
+        ]
     )
 
-    reproject = pyproj.Transformer.from_crs(
-        "EPSG:4326", "EPSG:27700", always_xy=True
-    ).transform 
+    reproject = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:27700", always_xy=True).transform
     aoi_27700 = transform(reproject, aoi)
     minx, miny, maxx, maxy = aoi_27700.bounds
 
@@ -24,8 +24,12 @@ def test_clip_small_dataset(ee_initialize):
     scale = 100  # meters
 
     affine = [
-        scale, 0, minx,
-        0, -scale, maxy,
+        scale,
+        0,
+        minx,
+        0,
+        -scale,
+        maxy,
     ]
 
     aoi_ee = ee.Geometry.Polygon(list(aoi.exterior.coords)[0:4])
@@ -41,16 +45,16 @@ def test_clip_small_dataset(ee_initialize):
     ds = xr.open_dataset(
         ic,
         engine="ee",
-        projection=ee.Projection("EPSG:27700",transform=affine),
+        projection=ee.Projection("EPSG:27700", transform=affine),
         geometry=list(aoi.bounds),
         scale=100,
     )
 
     ds = ds.to_array(dim="feature")
 
-    print ('materializing to zarr')
-    ds.to_zarr('test.zarr', mode='w')
+    print("materializing to zarr")
+    ds.to_zarr("test.zarr", mode="w")
 
-    z = zarr.open('test.zarr')
+    z = zarr.open("test.zarr")
 
     assert True
